10d01996e1d5a0d40338d73014e723003edd4304
the-27 <blackoficial2025@gmail.com>
2025-07-17 10:51:00 -0500

    𝖘𝖚𝖐𝖚𝖓𝖆  𝕸𝕯

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -106,7 +106,11 @@ global.icono = [
 'https://files.catbox.moe/qnvpyq.jpg'
 ].getRandom()
 
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
+/*global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}*/
+
+
+global.rcanal = {contextInfo: {forwardingScore: 2025, isForwarded: true, externalAdReply: {title: textbot, body: dev, sourceUrl: redes, thumbnailUrl: icono}}}
+}
 
 export default handler
 
