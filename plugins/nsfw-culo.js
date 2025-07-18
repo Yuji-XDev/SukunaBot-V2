48bd8386efc89617e5a87cf60abd8ec0d277798a
the-27 <blackoficial2025@gmail.com>
2025-07-18 07:07:25 -0500

    Sukuna MD

--- plugins/nsfw-culo.js
+++ plugins/nsfw-culo.js
@@ -1,15 +1,21 @@
-let handler = async(m, { conn }) => {
+let handler = async (m, { conn }) => {
 
-let img = 'https://dark-core-api.vercel.app/api/random/ass';
+  let img = 'https://dark-core-api.vercel.app/api/random/ass?key=api';
+  let text = '🍑 *Disfruta tu ración de... arte digital 🙈*';
 
-let text = '🍑 *Disfruta tu ración de... arte digital 🙈*';
+  conn.sendMessage(m.chat, {
+    image: { url: img },
+    caption: text,
+    buttons: [
+      { buttonId: '.culo', buttonText: { displayText: '🔁 Otra más' }, type: 1 }
+    ]
+  }, { quoted: m });
 
-conn.sendMessage(m.chat, { image: { url: img }, caption: text }, { quoted: m });
-m.react('✅');
+  m.react('✅');
 }
 
-handler.help = ['tetas'];
+handler.help = ['culo'];
 handler.tags = ['nsfw'];
-handler.command = ['tetas'];
+handler.command = ['culo'];
 
 export default handler;
