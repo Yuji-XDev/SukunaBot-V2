d9cfbfbe939a87b27e61bc5846bd70b16c2835e0
the-27 <blackoficial2025@gmail.com>
2025-07-17 20:26:03 -0500

    Sukuna MD

--- plugins/nsfw-culo.js
+++ plugins/nsfw-culo.js
@@ -1,35 +1,33 @@
 import fetch from 'node-fetch';
 
-let handler = async(m, { conn, usedPrefix, command }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
+  m.react('🕑');
 
-m.react('🕑');
+  const gp = global.db.data.chats[m.chat] || {};
+  if (!gp.nsfw && m.isGroup) return m.reply('[❗] *El contenido `NSFW` está desactivado en este grupo.*\n> Un administrador puede activarlo con el comando » *#nsfw on*');
 
-const gp = global.db.data.chats[m.chat] || {};
+  let txt = '🍑 *Disfruta tu ración de... arte digital 🙈*';
+  let img = 'https://dark-core-api.vercel.app/api/random/ass?key=api';
 
-if (!gp.nsfw && m.isGroup) return m.reply('[❗] *El contenido \`NSFW\` está desactivado en este grupo.*\n> Un administrador puede activarlo con el comando » *#nsfw on*');
-
-let txt = '🍑 *Disfruta tu ración de... arte digital 🙈*';
-
-let img = 'https://dark-core-api.vercel.app/api/random/ass?key=api';
-
-m.react('✅');
-conn.sendMessage(m.chat, { 
-        image: { url: img }, 
-        caption: txt, 
-        footer: dev, 
-        buttons: [
-            {
-                buttonId: '.culo',
-                buttonText: { displayText: '⏩ Siguiente' }
-            }
-        ],
-        viewOnce: true,
-        headerType: 4
-    }, { quoted: m });
-}
+  m.react('✅');
+  
+  await conn.sendMessage(m.chat, {
+    image: { url: img },
+    caption: txt,
+    footer: '🔥 Sukuna Bot MD',
+    buttons: [
+      {
+        buttonId: '#culo',
+        buttonText: { displayText: '⏩ Siguiente' },
+        type: 1
+      }
+    ],
+    headerType: 4
+  }, { quoted: m });
+};
 
 handler.help = ['culo'];
-handler.tag = ['nsfw'];
+handler.tags = ['nsfw'];
 handler.command = ['culo'];
 
 export default handler;
