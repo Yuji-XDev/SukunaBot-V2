04ebd83b630aa3d84a81be5f2819cee3af2db1f3
the-27 <blackoficial2025@gmail.com>
2025-07-20 18:47:35 -0500

    Update

--- plugins/menu-prueba.js
+++ plugins/menu-prueba.js
@@ -25,12 +25,12 @@ let handler = async (m, { conn }) => {
     );
 
     const listaComandos = comandosBusqueda.map(plugin => {
-      return plugin.help.map(cmd => `*യ ׄ🌳˚ .${toAesthetic(cmd)}*`).join('\n');
+      return plugin.help.map(cmd => `*യ ׄ🌳˚ .${toAesthetic(cmd)}* (on/off)`).join('\n');
     }).join('\n');
 
  
     const texto = `
-${listaComandos } (on/off)
+${listaComandos }
 
 > ${global.club || '👑 ʙᴏᴛ ᴘᴏʀ ʙʟᴀᴄᴋ'}
 `.trim();
